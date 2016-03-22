9fc890b55d02f7ca1feadbb568b66520797c65b3
robynsmith2786 <robynsmith2786@gmail.com>
2016-03-22 10:20:52 -0400

    kjn

--- js/getNav.js
+++ js/getNav.js
@@ -1,12 +1,12 @@
 $.get("http://robynsmith2786.github.io/partials/nav.html", function(data) {
 
-  $(document).ready(function() {
-	//check if this document has .container
-	var container = $(".container");
-	//if so add nav.html contents to the top
-	if (container) {
-  	container.prepend(data)
-  	//container.fadeIn();
-	}
-  });
+    $(document).ready(function() {
+        //check if this document has .container
+        var container = $(".container");
+        //if so add nav.html contents to the top
+        if (container) {
+            container.prepend(data)
+                //container.fadeIn();
+        }
+    });
 });
