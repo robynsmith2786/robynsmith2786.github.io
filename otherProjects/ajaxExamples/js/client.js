fa07373030c01d26037492f000a341ee4fa3146c
robynsmith2786 <robynsmith2786@gmail.com>
2016-03-22 11:23:13 -0400

    vv

--- otherProjects/ajaxExamples/js/client.js
+++ otherProjects/ajaxExamples/js/client.js
@@ -1,17 +1,29 @@
-$.getJSON("jsonDatabase/clients.json", function (data) {
-	
-$(document).ready(function () {
-    
-	var html = "";
-    	
-$.each(data, function (index, item) {
-        	html += "<div>" +
-            	"<span class='song'>" + item.name + "</span>" +
-            	"<span class='artist'>" + item.artist + "</span>" +
-            	"</div>"
-    	});
+$(document).ready(function() {
 
-    	$("#").append(html);
+    $("#getClients").on("click", function() {
 
-	});
-});
+        var url = "http://robynsmith2786.github.io/otherProjects/ajaxExamples/jsonDatabase/clients.json";
+
+        $.getJSON(url, function(data) {
+            var html = "<table class='table table-hover table-striped'>" +
+              "<tr><th>Name</th><th>Email</th><th>Company</th></tr>";
+
+            $.each(data, function(index, item) {
+
+              html += "<tr>" +
+                "<td>" + item.name + "</td>" +
+                "<td>" + item.email + "</td>" +
+                "<td>" + item.company + "</td>" +
+                "</tr>";
+            })
+
+            html += "</table>";
+            $("#data").append(html);
+            //alert(data);
+            //console.dir(data);
+
+          }) //getJSON
+
+      }) //click
+
+  }) //ready
