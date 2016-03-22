739ec05ca04393f71869d98dee42bba6b9759965
robynsmith2786 <robynsmith2786@gmail.com>
2016-03-22 12:47:09 -0400

    mb

--- otherProjects/ajaxExamples/js/dogs.js
+++ otherProjects/ajaxExamples/js/dogs.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
     $("#getBreeds").on("click", function() {
-        console.log("hello");
+       
         var url = "http://robynsmith2786.github.io/otherProjects/ajaxExamples/jsonDatabase/dogs.json";
 
         $.getJSON(url, function(data) {
-            
+            console.log(data);
             var html= '';
             $.each(data, function(index, item) {
                 html += '<div class="row"><div class="col-xs-12"><h1>'+ item.breed +'</h1><img src="'+ item.image +'">              <p>'+item.description+'</p></div></div>';
