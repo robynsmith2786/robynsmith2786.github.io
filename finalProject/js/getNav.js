a84ab8cd51a45a621980f75e6c35feecea6725cf
robynsmith2786 <robynsmith2786@gmail.com>
2016-04-01 14:33:20 -0400

    nav

---
+++ finalProject/js/getNav.js
@@ -0,0 +1,21 @@
+
+$.get("http://robynsmith2786.github.io/partials/nav.html", function(data) {
+
+    $(document).ready(function() {
+        //check if this document has .container
+        var container = $(".container");
+        //if so add nav.html contents to the top
+        if (container) {
+            container.prepend(data)
+            container.fadeIn();
+            $("#"+page).addClass('active');
+        }
+    });
+});
+
+$.get("http://robynsmith2786.github.io/partials/footer.html",function(foot){
+  $(document).ready(function(){
+    $(".container").append(foot);
+      $(".container").fadeIn();
+  })  
+})
